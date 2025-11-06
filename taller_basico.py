b7ccf0619364e005aeb9c78ed1c098aa8cba5f2b
Santiago Chavarro <santiagoachm@gmail.com>
2025-11-06 13:43:34 -0500

    taller_basico

---
+++ taller_basico.py
@@ -0,0 +1,17 @@
+
+# NIVEL 1
+#1.1 se solicita el nombre y la edad al usuario con input imprimiendo al final la información recopilada
+
+print("hola, ingresa tu nombre: ")
+nombre = input()
+nacimiento = int(input("ingresa tu edad: "))
+
+print(f"hola {nombre}, tienes {nacimiento} de edad")
+
+
+#1.2 Se realiza una suma simple según lo indica el ejercicio
+
+A = 15
+B = 19
+
+print(A+B)
