358867fa6d187a9b5893c616357da7d7926304da
Santiago Chavarro <santiagoachm@gmail.com>
2025-11-07 07:32:31 -0500

    cambios 3

--- taller_basico.py
+++ taller_basico.py
@@ -1,6 +1,6 @@
 
 # NIVEL 1
-#1.1 se solicita el nombre y la edad al usuario con input imprimiendo al final la información recopilada
+# 1.1 se solicita el nombre y la edad al usuario con input imprimiendo al final la información recopilada
 
 print("hola, ingresa tu nombre: ")
 nombre = input()
@@ -9,14 +9,14 @@ nacimiento = int(input("ingresa tu edad: "))
 print(f"hola {nombre}, tienes {nacimiento} de edad")
 
 
-#1.2 Se realiza una suma simple según lo indica el ejercicio
+# 1.2 Se realiza una suma simple según lo indica el ejercicio
 
 A = 15
 B = 19
 
-print(A+B)
+# print(A+B)
 
-#1.3 pedir datos al usuario
+# 1.3 pedir datos al usuario
 
 base = float(input("ingrese la base del triángulo: "))
 altura = float(input("ingrese la altura del triángulo: "))
@@ -26,5 +26,18 @@ area= (base * altura) / 2
 
 print(f"el área del triangulo es: {area}")
 
-# 1.4
+# 1.4 se utiliza para calcular un input para ingresar la temperatura y el tipo de dato que queremos, en este caso float
 
+celsius = float(input("introduce la temperatura: "))
+fahrenheit = (celsius * 9/5) +32
+
+print(f"{celsius} grados celsius equivalen a {fahrenheit} grados fahrenheit: ")
+
+#1.5
+
+T1 = 23985
+T2 = 239.85
+T3 = "BMW F 850 GS ADV"
+T4 = True
+T5 = ["manzana", 12, 15.6, "celular"]
+T6 = 
