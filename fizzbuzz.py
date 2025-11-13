27c947949cfd653ef53c7953520e824eba1dafe6
Santiago Chavarro <santiagoachm@gmail.com>
2025-11-13 12:38:50 -0500

    Primer commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,15 @@
+import time
 for variable in range(1, 101):
 
     if (variable % 5 == 0) and (variable % 3 == 0):
         print("FizzBuzz")
+        time.sleep(.2)
     elif variable % 3 == 0:
         print("Fizz")
+        time.sleep(.2)
     elif variable % 5 == 0:
         print("Buzz")
+        time.sleep(.2)
     else:
         print(variable)
-    
-    
+        time.sleep(.2)
